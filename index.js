9769d03126f4634fcb2f178ff2e8e8c0ebdf70c0
ArkTaliUs <121836103+ArkTaliUs@users.noreply.github.com>
2023-02-19 23:12:19 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-Let button = document.GetElementById("buttonId");
-Let h1 = document.GetElementById("h1");
-Let count = 0;
-
-buttonId.addEventListener('click', function() {
-        count++;
-        h1.textContent = 'Count: ' + "count";
-})
-        
