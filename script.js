64bbd0ef03527e840bb9bc231dbc41b78089c287
ArkTaliUs <121836103+ArkTaliUs@users.noreply.github.com>
2023-02-19 20:29:40 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-Let buttonId = document.GetElementById('buttonId');
-Let h1 = document.GetElementById('h1);
-Let count = 0;
-
-ButtonId.addEventListener('click', function() {
-        count++;
-        h1.textContent = 'Counter: ' + count;
-})
-        
