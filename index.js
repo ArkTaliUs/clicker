7fc64513fbb7fc00985106b3184138f0404d62d9
ArkTaliUs <121836103+ArkTaliUs@users.noreply.github.com>
2023-02-19 20:45:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ Let button = document.GetElementById('buttonId');
 Let h1 = document.GetElementById('h1');
 Let count = 0;
 
-Button.addEventListener('click', function() {
+ButtonId.addEventListener('click', function() {
         count++;
         h1.textContent = 'Count: ' + count;
 })
