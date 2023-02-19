3d0903c55bfe7128cb31dd58f857022eb74a90ed
ArkTaliUs <121836103+ArkTaliUs@users.noreply.github.com>
2023-02-19 20:18:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-Let button = document.GetElementById('button');
+Let buttonId = document.GetElementById('buttonId');
 Let h1 = document.GetElementById('h1);
 Let count = 0;
 
-Button.addEventListener('click', function() {
+ButtonId.addEventListener('click', function() {
         count++;
         h1.textContent = 'Counter: ' + count;
 )}
